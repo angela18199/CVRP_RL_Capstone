1833dc1251514662bf8845e2a90946b02f08ff78
th264 <tzuchun.hsieh@duke.edu>
2021-01-31 16:58:21 -0500

    fix code

--- train.py
+++ train.py
@@ -65,7 +65,7 @@ def clip_grad_norms(param_groups, max_norm=math.inf):
     return grad_norms, grad_norms_clipped
 
 
-def train_epoch(model, optimizer, baseline, lr_scheduler, epoch, val_dataset, problem, tb_logger, opts, start_start=0, config):
+def train_epoch(model, optimizer, baseline, lr_scheduler, epoch, val_dataset, problem, tb_logger, opts, config, start_start=0):
     print("Start train epoch {}, lr={} for run {}".format(epoch, optimizer.param_groups[0]['lr'], opts.run_name))
     step = epoch * (opts.epoch_size // config.batch_size)
     start_time = time.time()
